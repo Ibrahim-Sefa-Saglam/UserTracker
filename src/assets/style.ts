8540c09f437bfca038cbc57ce77aebe9ee1e2e0e
Ibrahim-Sefa-Saglam <saglamibrahimsefa@gmail.com>
2025-09-06 12:28:01 +0300

    added style.ts

---
+++ src/assets/style.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  gray: "#5C6672",
+  black: "#26303E",
+  border: "#26303E",
+  purple: "#4F359B",
+}
+
+export const fontWeights = {
+  light: "300",
+  normal: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+}
