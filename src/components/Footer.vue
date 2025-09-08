dbc7d53bc7739bbe912aa5189cfb1a8bf08ca1c2
Ibrahim-Sefa-Saglam <saglamibrahimsefa@gmail.com>
2025-09-08 11:04:22 +0300

    auto commit

--- src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="bg-gray-100 text-center p-2 text-sm text-gray-600">
-    © 2025 UserTracker
-  </footer>
-</template>
--- src/components/Header.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="bg-white  px-10 py-8 text-xl font-bold">
-    <slot />
-  </header> 
-</template>  
